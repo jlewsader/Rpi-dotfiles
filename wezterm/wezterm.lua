675a9174ac44086371d3ebacd143e557507ee82f
Justin Lewsader <jlewsader@gmail.com>
2025-12-31 14:56:06 -0500

    comment out window resize decorations

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,5 +3,5 @@ local wezterm = require("wezterm")
 return {
 	color_scheme = "Dracula",
 	font_size = 12.0,
-	window_decorations = "RESIZE",
+	--	window_decorations = "RESIZE", --Uncomment for window resize buttons
 }
